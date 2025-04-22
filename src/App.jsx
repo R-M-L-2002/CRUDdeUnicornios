bd56b584acc806c2ce8eedf03b1bff37631c8329
Rebeca Lopez <r.lopez@itecriocuarto.org.ar>
2025-04-22 18:20:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, Fragment } from 'react'
 import UnicornsContainer from './unicorns'
 import './App.css'
-import 'primereact/resources/themes/lara-light-blue/theme.css'; // Puedes elegir otro tema si quieres
+import 'primereact/resources/themes/lara-light-blue/theme.css'; 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
